b0fed722a7d7f14e3ccff64e40c6ee0a4c456265
Bryan Lee <bryanrichie97@gmail.com>
2023-07-12 22:40:11 +1000

    feature: advice generator app base design

--- src/pages/AdviceGeneratorApp.tsx
+++ src/pages/AdviceGeneratorApp.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { Flex, Spinner, Text } from '@chakra-ui/react';
+import { Box, Flex, Image, Spinner, Text, VStack } from '@chakra-ui/react';
+import '../assets/AdviceGeneratorApp/css/font.css';
 
 interface AdviceSlip {
   slip: {
@@ -21,16 +22,31 @@ const AdviceGeneratorApp = () => {
   }, []);
 
   return (
-    <>
+    <Flex
+      w={'1440px'}
+      h={'100vh'}
+      backgroundColor={'hsl(218, 23%, 16%)'}
+      align={'center'}
+      justify={'center'}
+    >
       {!advice ? (
         <Spinner />
       ) : (
-        <Flex>
-          <Text>{advice.slip.id}</Text>
-          <Text>{advice.slip.advice}</Text>
-        </Flex>
+        <VStack backgroundColor={'hsl(217, 19%, 24%)'} fontFamily={'Manrope'}>
+          <Text color={'hsl(150, 100%, 66%)'}>ADVICE #{advice.slip.id}</Text>
+          <Text fontSize={'28px'} color={'hsl(193, 38%, 86%)'}>
+            "{advice.slip.advice}"
+          </Text>
+          <Image src="/src/assets/AdviceGeneratorApp/pattern-divider-desktop.svg" />
+          <Image
+            src="/src/assets/AdviceGeneratorApp/icon-dice.svg"
+            borderRadius={'full'}
+            backgroundColor={'hsl(150, 100%, 66%)'}
+            padding={'5'}
+          />
+        </VStack>
       )}
-    </>
+    </Flex>
   );
 };
 
